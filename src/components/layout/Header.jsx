0c4e1d0ae693b0dbd8e82826cc72378f7974c938
cmccormack <mccormack.christopher@gmail.com>
2017-09-15 12:02:42 -0700

    Added proptypes and defaultprops, replace NavItem with ListItemLink for react-router 4

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,32 @@
 /* eslint no-console: 'off' */
 
 import React from 'react'
-import 'font-awesome/css/font-awesome.css'
+import PropTypes from 'prop-types'
 
 // Replaced NavItem from modules due to bug in NavItem
-import Navbar from 'react-bootstrap/lib/NavBar'
+import Navbar from 'react-bootstrap/lib/Navbar'
 import Nav from 'react-bootstrap/lib/Nav'
 import MenuItem from 'react-bootstrap/lib/MenuItem'
 import NavDropdown from 'react-bootstrap/lib/NavDropdown'
+// import { Link } from 'react-router-dom'
+// import NavItem from '../NavItem'
+import ListItemLink from './ListItemLink'
 
-import NavItem from '../NavItem'
 
+function buildMenuItem(item, eventKeys) {
+  const dropdown = eventKeys.navCollapse.navDropdown
+  dropdown.menuItemsCount += 1
+  const eventKey = +`${dropdown.self}.${dropdown.menuItemsCount}`
+  return (
+    <MenuItem
+      eventKey={eventKey}
+      href={item.url}
+      key={item.name}
+    >
+      {item.name}
+    </MenuItem>
+  )
+}
 
 function Header(props) {
   const { brand, navItems, dropdown } = props
@@ -46,17 +62,23 @@ function Header(props) {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav pullRight>
-          {navItems.map(item => (
-            <NavItem
-              eventKey={++eventKeys.navCollapse.navItemsCount}
-              href={item.url}
-              key={item.name}
-            >
-              {item.name}
-            </NavItem>
-          ))}
-          <NavDropdown
-            eventKey={++eventKeys.navCollapse.navItemsCount}
+          {navItems.map((item) => {
+            eventKeys.navCollapse.navItemsCount += 1
+            return (
+              <ListItemLink
+                eventKey={eventKeys.navCollapse.navItemsCount}
+                to={item.url}
+                key={item.name}
+              >
+                {item.name}
+              </ListItemLink>
+            )
+          })}
+          {dropdown && <NavDropdown
+            eventKey={() => {
+              eventKeys.navCollapse.navItemsCount += 1
+              return eventKeys.navCollapse.navItemsCount
+            }}
             id={dropdown.id}
             title={dropdown.title}
           >
@@ -67,25 +89,27 @@ function Header(props) {
             {
               dropdown.footer.map(item => buildMenuItem(item, eventKeys))
             }
-          </NavDropdown>
+          </NavDropdown>}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
 
-function buildMenuItem(item, eventKeys) {
-  const dropdown = eventKeys.navCollapse.navDropdown
-  const eventKey = +`${dropdown.self}.${++dropdown.menuItemsCount}`
-  return (
-    <MenuItem
-      eventKey={eventKey}
-      href={item.url}
-      key={item.name}
-    >
-      {item.name}
-    </MenuItem>
-  )
+Header.propTypes = {
+  brand: PropTypes.shape(),
+  navItems: PropTypes.arrayOf(PropTypes.object),
+  dropdown: PropTypes.shape(),
+}
+
+Header.defaultProps = {
+  brand: {
+    title: 'Title',
+    icon: { class: 'fa fa-lg fa-fw fa-times', height: '0.7' },
+    url: '#',
+  },
+  navItems: [],
+  dropdown: null,
 }
 
 export default Header
